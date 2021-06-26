5ac9b98ef14edb560c590ebc69d2edf07367ef91
Ferry Temaja Putra <ferry_temajaputra@technologyonecorp.com>
2021-06-26 13:21:36 +0800

    Add project files.

---
+++ HelloUno/HelloUno.Shared/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HelloUno
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<Todo> TodoList { get; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            TodoList = new ObservableCollection<Todo>();
+        }
+
+        private void RecordButton_Click(object sender, RoutedEventArgs e)
+        {
+            TodoList.Add(new Todo
+            {
+                TodoId = Guid.NewGuid().ToString(),
+                Description = TodoText.Text,
+                CreatedDate = DateTime.Now,
+                Done = false
+            });
+
+            TodoText.Text = "";
+        }
+    }
+}
---
+++ HelloUno/HelloUno.Shared/Todo.cs
@@ -0,0 +1,39 @@
+﻿using MvvmHelpers;
+using System;
+
+namespace HelloUno
+{
+    public class Todo : ObservableObject
+    {
+        private string _todoId;
+
+        public string TodoId 
+        {
+            get => _todoId;
+            set => SetProperty(ref _todoId, value);
+        }
+
+        private DateTime _createDate;
+
+        public DateTime CreatedDate
+        {
+            get => _createDate;
+            set => SetProperty(ref _createDate, value);
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        private bool _done;
+
+        public bool Done
+        {
+            get => _done;
+            set => SetProperty(ref _done, value);
+        }
+    }
+}
